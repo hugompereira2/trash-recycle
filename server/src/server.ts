7f6d4ed252a7a74601e2c7a50446bc6266c2312b
hugompereira2 <55194253+hugompereira2@users.noreply.github.com>
2023-03-20 17:47:45 -0300

    Revert "serverless config"
    This reverts commit e97ca8b93376a04a63cf66ea4e1dc438ced1b459.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,22 +2,13 @@ import Fastify from "fastify";
 import cors from "@fastify/cors"
 import { appRoutes } from "./routes";
 
-export function init() {
-    const app = Fastify();
-    app.register(cors);
-    app.register(appRoutes)
+const app = Fastify();
 
-    app.get('/', (request, reply) => reply.send({ hello: 'world' }));
-    return app;
-}
+app.register(cors);
+app.register(appRoutes)
 
-if (require.main === module) {
-    // called directly i.e. "node app"
-    init().listen({ port: 3000 }, (err) => {
-        if (err) console.error(err);
-        console.log('server listening on 3000');
-    });
-} else {
-    // required as a module => executed on aws lambda
-    module.exports = init;
-}
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log("HTTP Server Running")
+});
