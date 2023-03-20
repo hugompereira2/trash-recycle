e97ca8b93376a04a63cf66ea4e1dc438ced1b459
hugompereira2 <55194253+hugompereira2@users.noreply.github.com>
2023-03-20 16:39:25 -0300

    serverless config

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,22 @@ import Fastify from "fastify";
 import cors from "@fastify/cors"
 import { appRoutes } from "./routes";
 
-const app = Fastify();
+export function init() {
+    const app = Fastify();
+    app.register(cors);
+    app.register(appRoutes)
 
-app.register(cors);
-app.register(appRoutes)
+    app.get('/', (request, reply) => reply.send({ hello: 'world' }));
+    return app;
+}
 
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log("HTTP Server Running")
-});
+if (require.main === module) {
+    // called directly i.e. "node app"
+    init().listen({ port: 3000 }, (err) => {
+        if (err) console.error(err);
+        console.log('server listening on 3000');
+    });
+} else {
+    // required as a module => executed on aws lambda
+    module.exports = init;
+}
